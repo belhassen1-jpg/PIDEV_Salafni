b47fe96a3d0419ccad077410be0471429193c4c5
nader2112 <74879065+nader2112@users.noreply.github.com>
2023-02-20 22:51:42 +0100

    Add files via upload

---
+++ src/main/java/com/example/pidev_salafni/PidevSalafniApplication.java
@@ -0,0 +1,13 @@
+package com.example.pidev_salafni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevSalafniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PidevSalafniApplication.class, args);
+    }
+
+}
